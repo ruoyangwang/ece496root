f1d84fd60597670769f4042bbdf877b92200e2b5
Liu  Jie <liujie7@ug221.eecg>
2014-11-16 14:37:33 -0500

    Added WorkerObject to represent a worker

---
+++ src/WorkerObject.java
@@ -0,0 +1,32 @@
+public class WorkerObject {
+
+	// name of the worker. ie: dir name for this worker
+	public String workerName;
+
+	// performance metrics -- not yet used
+	public Integer benchmarkTime;
+
+	public final String DELIMITER = ":";
+
+	public Worker(String workerName) {
+		this.workerName = workerName;
+	}
+
+	// Get the name for the znode of this worker.
+	public String getNodeName() {
+		return workerName;
+	}
+
+	// TODO: this is not yet used		
+	// Parse the data string to get object representation
+	public void parseNodeString(String nodeDataString) {
+		benchmarkTime = Integer.parseInt(nodeDataString);
+	} 
+
+	// TODO: this is not yet used		
+	// Get the string representation of the data in the znode.
+	public String toNodeDataString() {
+		return benchmarkTime.toString();
+	}
+
+}
