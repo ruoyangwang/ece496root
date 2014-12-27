97fec6374b452472d3c48ca0fbca0296811e1d5d
ruoyang wang <leowry123@hotmail.com>
2014-12-27 13:59:13 -0500

    unit test, works

--- src/test.java
+++ src/test.java
@@ -6,16 +6,25 @@ import java.io.InputStreamReader;
 
 public class test{	
 	String temp;
+	private String workerName;
 	long executionTime;
-	
+	String cpucore;
+	String DELIMITER=":";
+
 	public static void main(String[] args){
 		test wk = new test();
-		wk.temp = wk.addToFreeWorker();
-		System.out.println(wk.temp);
-
+		wk.cpucore = wk.addToFreeWorker();
+		System.out.println(wk.cpucore);
+		System.out.println(wk.nodeToString());
 
 	}
 	
+
+	public test(){
+		this.workerName= "test 1";
+		this.cpucore=null;
+	}
+
 	public String addToFreeWorker(){
 		
 		try{
@@ -32,6 +41,11 @@ public class test{
             e.printStackTrace();
             return null;
         }
+
+	}
+
+	public String nodeToString(){
+		return this.workerName+ this.DELIMITER+ this.cpucore+ this.DELIMITER+this.executionTime;
 
 	}
 }
