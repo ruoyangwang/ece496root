aa44781b68f81278361c40f6f0605ba743674c31
Liu  Jie <liujie7@ug175.eecg>
2015-02-15 17:01:11 -0500

    source profile when starting script

--- execute/startWorkers.sh
+++ execute/startWorkers.sh
@@ -2,5 +2,5 @@
 
 # 1 = host , 2 = zookeeper host 3 = input file 4 = jobId
 
-ssh $1 "cd ~/ece496root/bin && nohup ./worker.sh $2 $3 $4 >> ~/worker.$1.log &"
+ssh -i ~/.ssh/pass $1 "source ~/.bash_profile && cd ~/ece496root/bin && nohup ./worker.sh $2 $3 $4 >> ~/worker.$1.log &"
 # ssh $1 "cd ~/Desktop/ece496/ece496root/bin && nohup ./worker.sh $2 $3 $4 >> ~/worker.$1.log &"
